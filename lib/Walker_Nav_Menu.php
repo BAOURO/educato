2a92860b781a27170e7162f9ea3668077c5349ac
HASSANA DAOUDA Daouda <daoudahassan@gmail.com>
2025-09-07 18:22:47 +0100

    Fix Walker Nav Menu property access errors

--- lib/Walker_Nav_Menu.php
+++ lib/Walker_Nav_Menu.php
@@ -7,13 +7,30 @@ class Walker_Nav_Menu extends \Walker_Nav_Menu {
     public function start_el(&$output, $item, $depth = 0, $args = null, $id = 0) {
         $indent = ($depth) ? str_repeat("\t", $depth) : '';
         
+        // Convert args to object if it's an array
+        if (is_array($args)) {
+            $args = (object) $args;
+        }
+        
+        // Ensure args is an object with default properties
+        if (!is_object($args)) {
+            $args = new stdClass();
+        }
+        
+        // Set default properties if they don't exist
+        if (!isset($args->before)) $args->before = '';
+        if (!isset($args->after)) $args->after = '';
+        if (!isset($args->link_before)) $args->link_before = '';
+        if (!isset($args->link_after)) $args->link_after = '';
+        if (!isset($args->mobile)) $args->mobile = false;
+        
         $classes = empty($item->classes) ? [] : (array) $item->classes;
         $classes[] = 'menu-item-' . $item->ID;
         
         $class_names = join(' ', apply_filters('nav_menu_css_class', array_filter($classes), $item, $args));
         
         // Mobile menu classes
-        if (isset($args->mobile) && $args->mobile) {
+        if ($args->mobile) {
             $class_names = 'block px-4 py-3 text-gray-700 hover:bg-gray-50 hover:text-primary-600 transition-colors';
             if ($depth > 0) {
                 $class_names .= ' pl-8 text-sm';
@@ -36,21 +53,23 @@ class Walker_Nav_Menu extends \Walker_Nav_Menu {
         $title = apply_filters('the_title', $item->title, $item->ID);
         
         // Link classes
-        if (isset($args->mobile) && $args->mobile) {
+        if ($args->mobile) {
             $link_class = 'block w-full text-left';
         } else {
             $link_class = 'block px-4 py-2 text-gray-700 hover:text-primary-600 transition-colors font-medium';
         }
         
-        $item_output = '<a class="' . $link_class . '"' . $attributes . '>';
-        $item_output .= $title;
+        $item_output = $args->before ?? '';
+        $item_output .= '<a class="' . $link_class . '"' . $attributes . '>';
+        $item_output .= ($args->link_before ?? '') . $title . ($args->link_after ?? '');
         
         // Add dropdown indicator for desktop
-        if (!isset($args->mobile) && in_array('menu-item-has-children', $classes)) {
+        if (!$args->mobile && in_array('menu-item-has-children', $classes)) {
             $item_output .= '<svg class="w-4 h-4 ml-1 inline-block group-hover:rotate-180 transition-transform" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>';
         }
         
         $item_output .= '</a>';
+        $item_output .= $args->after ?? '';
         
         $output .= $item_output;
     }
@@ -58,7 +77,22 @@ class Walker_Nav_Menu extends \Walker_Nav_Menu {
     public function start_lvl(&$output, $depth = 0, $args = null) {
         $indent = str_repeat("\t", $depth);
         
-        if (isset($args->mobile) && $args->mobile) {
+        // Convert args to object if it's an array
+        if (is_array($args)) {
+            $args = (object) $args;
+        }
+        
+        // Ensure args is an object with mobile property
+        if (!is_object($args)) {
+            $args = new stdClass();
+            $args->mobile = false;
+        }
+        
+        if (!isset($args->mobile)) {
+            $args->mobile = false;
+        }
+        
+        if ($args->mobile) {
             $output .= "\n$indent<ul class=\"ml-4\">\n";
         } else {
             $output .= "\n$indent<ul class=\"absolute left-0 top-full mt-1 w-48 bg-white rounded-lg shadow-lg border border-gray-100 opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-200 z-50\">\n";
