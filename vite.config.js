41483524b63042f03b73702b94f8ce2c1e0258d5
HASSANA DAOUDA Daouda <daoudahassan@gmail.com>
2025-09-08 05:47:11 +0100

    Updated package-lock.json

--- vite.config.js
+++ vite.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
   css: {
     postcss: {
       plugins: [
-        require('@tailwindcss/postcss'),
+        require('tailwindcss'),
         require('autoprefixer')
       ]
     }
